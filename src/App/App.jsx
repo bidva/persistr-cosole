cea468cb443323eba1a86f009f06f3abc8d2e6de
amir bidva <a.bidva@gmail.com>
2019-04-03 21:37:50 -0700

    login

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import {Router, Route, Redirect, Switch} from 'react-router-dom';
+import {Router, Route, Switch} from 'react-router-dom';
 import {history} from '../helpers';
 import {PrivateRoute} from "../components/PrivateRoute"
 import Account from "../components/Account/Account";
 import Login from "../components/Login/Login";
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,36 @@
 import React, {Component} from 'react';
-import {Container, Card, Col} from 'react-bootstrap';
+import {Container, Card, Col, Form, Button} from 'react-bootstrap';
 import logo from "../../assets/images/persistr-horizontal.png"
+import {history} from '../../helpers';
 
 export default class Login extends Component {
 
   static propTypes = {};
+  state = {
+    validated: false,
+    email:'',
+    password:''
+  };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {};
+  handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      localStorage.setItem('token',{})
+      history.push('/account');
+    };
+    this.setState({ validated: true });
   }
-
+  handleEmail = (e) => {
+    this.setState({ email: e.target.value })
+  }
+  handlePassword = (e) => {
+    this.setState({ password: e.target.value })
+  }
   render() {
+    const { validated, email, password } = this.state;
     return (<Container>
       <Col md={{
           span: 12,
@@ -29,11 +47,39 @@ export default class Login extends Component {
             }}>
             <div className="pt-5"></div>
             <img height={70} src={logo} alt="Kater"/>
-          </Col>
+            <Form
+            noValidate
+            validated={validated}
+            onSubmit={e => this.handleSubmit(e)}
+            className="mb-5 mt-5">
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control
+                  type="email"
+                  required
+                  placeholder="Enter email"
+                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                  onChange={this.handleEmail}
+                  value={email}
+                  />
+              </Form.Group>
 
+              <Form.Group controlId="formBasicPassword">
+                <Form.Control
+                  type="password"
+                  required
+                  minLength="8"
+                  placeholder="Password"
+                  onChange={this.handlePassword}
+                  value={password}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit" >
+                Submit
+              </Button>
+            </Form>;
+          </Col>
         </Card>
       </Col>
-
     </Container>);
   }
 
